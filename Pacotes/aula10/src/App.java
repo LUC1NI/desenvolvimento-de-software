e3e8f1feb9eda641c3e38c14a41c3523475be05f
JOAO VITOR LUCINI <joao.lucini@cs.up.edu.br>
2025-04-09 21:44:56 -0300

    Pacotes, Views, Modelos e Controllers

---
+++ Pacotes/aula10/src/App.java
@@ -0,0 +1,30 @@
+import controller.*;
+import java.util.Scanner;
+import model.*;
+import view.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello, World! MAIN" );
+        View.main(args);
+        Model.main(args);
+        Controller.main(args);
+
+        System.out.println(CidadeEnum.CAMPO_LARGO);
+
+        CidadeEnum cidade = CidadeEnum.CAMPO_LARGO;
+
+        System.out.println(cidade.getNome());
+
+        System.out.println("Digite uma cidade:");
+
+        CidadeEnum novaCidade= CidadeEnum.valueOf(
+            scanner.nextLine().trim().toUpperCase().replace(" ", "_")
+        );
+
+        System.out.println(novaCidade);
+
+
+    }   
+}
